9c51d5b67ff201be0b4df360b8bb38d3487fb667
alexey <yakovlev.aigo@gazprom-neft.ru>
2024-07-31 00:14:03 +0300

    SFML example

---
+++ SFML/TestApp/build/CMakeFiles/SfmlTest.Main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SfmlTest.Main.dir/src/main.cpp.o"
+  "CMakeFiles/SfmlTest.Main.dir/src/main.cpp.o.d"
+  "SfmlTest.Main"
+  "SfmlTest.Main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SfmlTest.Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
